0142c26214a49ba65e97e6a9270b31dbaefcd5c4
Chris Couzens <ccouzens@gmail.com>
2022-12-02 20:19:36 +0000

    Implement part 2

--- day_02/src/lib.rs
+++ day_02/src/lib.rs
@@ -1,14 +1,29 @@
+// Rock: 0
+// Paper: 1
+// Scissors: 2
+
+// Lose: 0
+// Draw: 1
+// Win: 2
+
 struct Round {
     theirs: u8,
     ours: u8,
 }
 
 impl Round {
-    fn new(input: &[u8]) -> Option<Self> {
+    fn new_with_their_move(input: &[u8]) -> Option<Self> {
         Some(Self {
             theirs: input.first()? - b'A',
             ours: input.get(2)? - b'X',
         })
+    }
+
+    fn new_with_round_outcome(input: &[u8]) -> Option<Self> {
+        let theirs = input.first()? - b'A';
+        let outcome = input.get(2)? - b'X';
+        let ours = (outcome + 2 + theirs) % 3;
+        Some(Self { ours, theirs })
     }
 
     fn score(&self) -> u64 {
@@ -19,8 +34,16 @@ impl Round {
 pub fn part_one(input: &str) -> u64 {
     input
         .lines()
-        .map(|line| Round::new(line.as_bytes()))
-        .map(|r| Round::score(&r.unwrap()))
+        .filter_map(|line| Round::new_with_their_move(line.as_bytes()))
+        .map(|r| Round::score(&r))
+        .sum()
+}
+
+pub fn part_two(input: &str) -> u64 {
+    input
+        .lines()
+        .filter_map(|line| Round::new_with_round_outcome(line.as_bytes()))
+        .map(|r| Round::score(&r))
         .sum()
 }
 
@@ -36,5 +59,15 @@ mod tests {
     #[test]
     fn challenge_part_one() {
         assert_eq!(part_one(include_str!("../challenge_1.txt")), 12855);
+    }
+
+    #[test]
+    fn example_part_two() {
+        assert_eq!(part_two(include_str!("../example_1.txt")), 12);
+    }
+
+    #[test]
+    fn challenge_part_two() {
+        assert_eq!(part_two(include_str!("../challenge_1.txt")), 13726);
     }
 }
