060e97578afdf0eb8ddac9964f659312a254d871
Chris Couzens <ccouzens@gmail.com>
2022-12-11 09:29:24 +0000

    Implement day 09 part 2
    a couple days late 😛

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -24,50 +24,6 @@ struct Motion {
     steps: usize,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Position {
-    x: usize,
-    y: usize,
-}
-
-impl Default for Position {
-    fn default() -> Self {
-        Self {
-            x: usize::MAX / 2,
-            y: usize::MAX / 2,
-        }
-    }
-}
-
-impl Position {
-    fn move_direction(&mut self, direction: Direction) {
-        match direction {
-            Up => self.y += 1,
-            Right => self.x += 1,
-            Down => self.y -= 1,
-            Left => self.x -= 1,
-        }
-    }
-
-    fn is_touching(&mut self, other: &Self) -> bool {
-        (self.x == other.x || self.x + 1 == other.x || self.x == other.x + 1)
-            && (self.y == other.y || self.y + 1 == other.y || self.y == other.y + 1)
-    }
-
-    fn move_towards(&mut self, other: &Self) {
-        match self.x.cmp(&other.x) {
-            Less => self.x += 1,
-            Equal => {}
-            Greater => self.x -= 1,
-        }
-        match self.y.cmp(&other.y) {
-            Less => self.y += 1,
-            Equal => {}
-            Greater => self.y -= 1,
-        }
-    }
-}
-
 impl Motion {
     fn iter(&self) -> impl Iterator<Item = Direction> {
         repeat(self.direction).take(self.steps)
@@ -90,20 +46,70 @@ fn parse_motion(input: &str) -> IResult<&str, Motion> {
     )(input)
 }
 
-pub fn part_one(input: &str) -> usize {
-    let mut motions_iterator = iterator(input, terminated(parse_motion, line_ending));
-    let mut head = Position::default();
-    let mut tail = head;
-    let mut visited = BTreeSet::new();
-    visited.insert(tail);
-    for direction in &mut motions_iterator.flat_map(|m| m.iter()) {
-        head.move_direction(direction);
-        if !tail.is_touching(&head) {
-            tail.move_towards(&head);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
+struct Position {
+    x: i16,
+    y: i16,
+}
+
+impl Position {
+    fn move_direction(&mut self, direction: Direction) {
+        match direction {
+            Up => self.y += 1,
+            Right => self.x += 1,
+            Down => self.y -= 1,
+            Left => self.x -= 1,
         }
-        visited.insert(tail);
+    }
+
+    fn is_touching(&mut self, other: &Self) -> bool {
+        (-1..=1).contains(&(self.x - other.x)) && (-1..=1).contains(&(self.y - other.y))
+    }
+
+    fn move_towards(&mut self, other: &Self) {
+        self.x += match self.x.cmp(&other.x) {
+            Less => 1,
+            Equal => 0,
+            Greater => -1,
+        };
+        self.y += match self.y.cmp(&other.y) {
+            Less => 1,
+            Equal => 0,
+            Greater => -1,
+        }
+    }
+}
+
+fn rope_simulation(input: &str, rope_length: usize) -> usize {
+    let mut motions_iterator = iterator(input, terminated(parse_motion, line_ending));
+    let mut rope = vec![Position::default(); rope_length];
+    let mut visited = BTreeSet::new();
+    for direction in &mut motions_iterator.flat_map(|m| m.iter()) {
+        let mut previous_knot = None;
+        for knot in rope.iter_mut() {
+            match previous_knot {
+                None => knot.move_direction(direction),
+                Some(previous_knot) => {
+                    if !knot.is_touching(previous_knot) {
+                        knot.move_towards(previous_knot);
+                    }
+                }
+            };
+            previous_knot = Some(knot);
+        }
+        if let Some(last_knot) = previous_knot {
+            visited.insert(*last_knot);
+        }
     }
     visited.len()
+}
+
+pub fn part_one(input: &str) -> usize {
+    rope_simulation(input, 2)
+}
+
+pub fn part_two(input: &str) -> usize {
+    rope_simulation(input, 10)
 }
 
 #[cfg(test)]
@@ -112,11 +118,26 @@ mod tests {
 
     #[test]
     fn example_part_one() {
-        assert_eq!(part_one(include_str!("../example.txt")), 13);
+        assert_eq!(part_one(include_str!("../example1.txt")), 13);
     }
 
     #[test]
     fn challenge_part_one() {
         assert_eq!(part_one(include_str!("../challenge.txt")), 5735);
+    }
+
+    #[test]
+    fn example_one_part_two() {
+        assert_eq!(part_two(include_str!("../example1.txt")), 1);
+    }
+
+    #[test]
+    fn example_two_part_two() {
+        assert_eq!(part_two(include_str!("../example2.txt")), 36);
+    }
+
+    #[test]
+    fn challenge_part_two() {
+        assert_eq!(part_two(include_str!("../challenge.txt")), 2478);
     }
 }
