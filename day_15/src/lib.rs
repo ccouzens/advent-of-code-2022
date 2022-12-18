5338b566ea00bbfe1f64df38f235daa31e1ef77b
Chris Couzens <ccouzens@gmail.com>
2022-12-18 22:36:15 +0000

    Implement day 15 part 2
    Even with the optimization it is slow.
    
    But I imagine far faster than if I hadn't made the change

--- day_15/src/lib.rs
+++ day_15/src/lib.rs
@@ -10,13 +10,13 @@ use nom::{
 
 #[derive(Debug, Clone, Copy)]
 struct Location {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Location {
     fn parse_nom(input: &str) -> IResult<&str, Self> {
-        fn parse_num(input: &str) -> IResult<&str, i32> {
+        fn parse_num(input: &str) -> IResult<&str, i64> {
             map_res(
                 take_while1(|c: char| c == '-' || ('0'..='9').contains(&c)),
                 str::parse,
@@ -28,14 +28,14 @@ impl Location {
         )(input)
     }
 
-    fn manhatten_distance(&self, other: &Self) -> u32 {
+    fn manhatten_distance(&self, other: &Self) -> u64 {
         self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
     }
 
-    fn manhatten_points_at_row(&self, distance: u32, row: i32) -> RangeInclusive<i32> {
+    fn manhatten_points_at_row(&self, distance: u64, row: i64) -> RangeInclusive<i64> {
         let vertical_distance = self.y.abs_diff(row);
-        let horizontal_distance: i32 = TryInto::<i32>::try_into(distance).unwrap()
-            - TryInto::<i32>::try_into(vertical_distance).unwrap(); // can be negative, which would give a empty range
+        let horizontal_distance: i64 = TryInto::<i64>::try_into(distance).unwrap()
+            - TryInto::<i64>::try_into(vertical_distance).unwrap(); // can be negative, which would give a empty range
         (self.x - horizontal_distance)..=(self.x + horizontal_distance)
     }
 }
@@ -69,14 +69,14 @@ impl Sensor {
         iterator(input, terminated(Self::parse_nom, newline))
     }
 
-    fn beacon_distance(&self) -> u32 {
+    fn beacon_distance(&self) -> u64 {
         self.location.manhatten_distance(&self.beacon)
     }
 }
 
-fn normalize_ranges(mut ranges: Vec<RangeInclusive<i32>>) -> Vec<RangeInclusive<i32>> {
+fn normalize_ranges(mut ranges: Vec<RangeInclusive<i64>>) -> Vec<RangeInclusive<i64>> {
     ranges.sort_by_key(|r| *r.start());
-    let mut normalized: Vec<RangeInclusive<i32>> = Vec::new();
+    let mut normalized: Vec<RangeInclusive<i64>> = Vec::new();
     for new_range in ranges.drain(..) {
         if new_range.is_empty() {
             continue;
@@ -84,7 +84,7 @@ fn normalize_ranges(mut ranges: Vec<RangeInclusive<i32>>) -> Vec<RangeInclusive<
         if let Some(normalized_range) = normalized.last_mut() {
             if new_range.start() <= normalized_range.end() {
                 *normalized_range =
-                    *normalized_range.start()..=i32::max(*normalized_range.end(), *new_range.end())
+                    *normalized_range.start()..=i64::max(*normalized_range.end(), *new_range.end())
             } else {
                 normalized.push(new_range);
             }
@@ -95,11 +95,11 @@ fn normalize_ranges(mut ranges: Vec<RangeInclusive<i32>>) -> Vec<RangeInclusive<
     normalized
 }
 
-pub fn part_one(input: &str, row: i32) -> usize {
-    let mut no_beacon_ranges = Vec::new();
+pub fn part_one(input: &str, row: i64) -> usize {
+    let mut covered_ranges = Vec::new();
     let mut beacons_in_row = BTreeSet::new();
     for sensor in &mut Sensor::parse_all_iterator(input) {
-        no_beacon_ranges.push(
+        covered_ranges.push(
             sensor
                 .location
                 .manhatten_points_at_row(sensor.beacon_distance(), row),
@@ -108,13 +108,30 @@ pub fn part_one(input: &str, row: i32) -> usize {
             beacons_in_row.insert(sensor.beacon.x);
         }
     }
-    let no_beacon_ranges = normalize_ranges(no_beacon_ranges);
+    let covered_ranges = normalize_ranges(covered_ranges);
 
-    no_beacon_ranges
+    covered_ranges
         .iter()
         .map(|r| r.clone().count())
         .sum::<usize>()
         - beacons_in_row.len()
+}
+
+pub fn part_two(input: &str, search_limit: i64) -> i64 {
+    let sensors: Vec<Sensor> = Sensor::parse_all_iterator(input).collect();
+    for row in 0..=search_limit {
+        let covered_ranges = sensors
+            .iter()
+            .map(|s| s.location.manhatten_points_at_row(s.beacon_distance(), row))
+            .collect();
+        let covered_ranges = normalize_ranges(covered_ranges);
+        for r in covered_ranges.iter() {
+            if (-1..search_limit).contains(r.end()) {
+                return (*r.end() + 1) * 4000000 + row;
+            }
+        }
+    }
+    0
 }
 
 #[cfg(test)]
@@ -129,5 +146,18 @@ mod tests {
     #[test]
     fn challenge_part_one() {
         assert_eq!(part_one(include_str!("../challenge.txt"), 2000000), 4793062);
+    }
+
+    #[test]
+    fn example_part_two() {
+        assert_eq!(part_two(include_str!("../example.txt"), 20), 56000011);
+    }
+
+    #[test]
+    fn challenge_part_two() {
+        assert_eq!(
+            part_two(include_str!("../challenge.txt"), 4000000),
+            10826395253551
+        );
     }
 }
