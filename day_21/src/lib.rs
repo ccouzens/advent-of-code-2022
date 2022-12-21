2240d3f39a0d2feba67ca841d7826322e0b77626
Chris Couzens <ccouzens@gmail.com>
2022-12-21 23:23:09 +0000

    Implement day 21 part 2
    I had to work backwards

--- day_21/src/lib.rs
+++ day_21/src/lib.rs
@@ -15,6 +15,7 @@ enum Operation {
     Minus,
     Mul,
     Div,
+    Eq,
 }
 
 #[derive(Clone, Debug)]
@@ -39,6 +40,31 @@ impl<'a> Job<'a> {
                     Operation::Minus => Some(a - b),
                     Operation::Mul => Some(a * b),
                     Operation::Div => Some(a / b),
+                    Operation::Eq => None,
+                }
+            }
+        }
+    }
+
+    fn missing_value(&self, troop: &Troop, value: i64) -> Option<(i64, &str)> {
+        match self {
+            Job::Number(_) => None,
+            &Job::Dependent { a, op, b } => {
+                let a_val = troop.monkeys.get(a).and_then(|j| j.get_value(troop));
+                let b_val = troop.monkeys.get(b).and_then(|j| j.get_value(troop));
+                match (a_val, op, b_val) {
+                    (Some(_), _, Some(_)) => None,
+                    (None, _, None) => None,
+                    (Some(a_val), Operation::Plus, None) => Some((value - a_val, b)),
+                    (Some(a_val), Operation::Minus, None) => Some((a_val - value, b)),
+                    (Some(a_val), Operation::Div, None) => Some((a_val / value, b)),
+                    (Some(a_val), Operation::Mul, None) => Some((value / a_val, b)),
+                    (Some(a_val), Operation::Eq, None) => Some((a_val, b)),
+                    (None, Operation::Plus, Some(b_val)) => Some((value - b_val, a)),
+                    (None, Operation::Minus, Some(b_val)) => Some((value + b_val, a)),
+                    (None, Operation::Div, Some(b_val)) => Some((value * b_val, a)),
+                    (None, Operation::Mul, Some(b_val)) => Some((value / b_val, a)),
+                    (None, Operation::Eq, Some(b_val)) => Some((b_val, a)),
                 }
             }
         }
@@ -101,6 +127,33 @@ pub fn part_one(input: &str) -> Option<i64> {
     root.get_value(&troop)
 }
 
+pub fn part_two(input: &str) -> Option<i64> {
+    let mut troop = Troop::new(input);
+    troop.monkeys.remove("humn");
+    if let &Job::Dependent { a, b, .. } = troop.monkeys.get("root")? {
+        let a_val = troop.monkeys.get(a).and_then(|j| j.get_value(&troop));
+        let b_val = troop.monkeys.get(b).and_then(|j| j.get_value(&troop));
+        let mut job = &Job::Dependent {
+            a,
+            op: Operation::Eq,
+            b,
+        };
+        let mut value = a_val.or(b_val)?;
+        loop {
+            let value_monkey = job.missing_value(&troop, value)?;
+            value = value_monkey.0;
+            let monkey = value_monkey.1;
+            if monkey == "humn" {
+                break Some(value);
+            } else {
+                job = troop.monkeys.get(monkey)?;
+            }
+        }
+    } else {
+        None
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -115,6 +168,19 @@ mod tests {
         assert_eq!(
             part_one(include_str!("../challenge.txt")),
             Some(49288254556480)
+        );
+    }
+
+    #[test]
+    fn example_part_two() {
+        assert_eq!(part_two(include_str!("../example.txt")), Some(301));
+    }
+
+    #[test]
+    fn challenge_part_two() {
+        assert_eq!(
+            part_two(include_str!("../challenge.txt")),
+            Some(3558714869436)
         );
     }
 }
