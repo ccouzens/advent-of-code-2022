02b8f3caf8c9e1e7c754a32ff0cfaea8c7640355
Chris Couzens <ccouzens@gmail.com>
2022-12-25 02:25:52 +0000

    Day 20 part 2!

--- day_20/src/lib.rs
+++ day_20/src/lib.rs
@@ -1,35 +1,45 @@
 use std::iter::zip;
 
-fn parse(input: &str) -> Vec<i16> {
+fn parse(input: &str) -> Vec<i64> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-fn mix(input: &[i16]) -> Vec<i16> {
-    let mut order = (0..input.len() as i16).collect::<Vec<_>>();
-
+fn mix(input: &[i64], order: &mut Vec<i64>) -> Vec<i64> {
     for (i, &num) in zip(0.., input.iter()) {
-        let current_position = order.iter().position(|&o| o == i).unwrap() as i16;
-        let new_position = (current_position + num).rem_euclid(input.len() as i16 - 1);
-        match current_position.cmp(&new_position) {
-            std::cmp::Ordering::Less => {
-                order.insert(new_position as usize + 1, i);
-                order.remove(current_position as usize);
-            }
-            std::cmp::Ordering::Equal => {}
-            std::cmp::Ordering::Greater => {
-                order.remove(current_position as usize);
-                order.insert(new_position as usize, i);
-            }
+        let current_position = order.iter().position(|&o| o == i).unwrap() as i64;
+        let new_position = (current_position + num).rem_euclid(input.len() as i64 - 1);
+        if current_position != new_position {
+            order.remove(current_position as usize);
+            order.insert(new_position as usize, i);
         }
     }
 
     order.iter().map(|&i| input[i as usize]).collect()
 }
 
-pub fn part_one(input: &str) -> i16 {
-    let nums = mix(&parse(input));
-    let p = nums.iter().position(|&n| n == 0).unwrap();
-    nums[(p + 1000) % nums.len()] + nums[(p + 2000) % nums.len()] + nums[(p + 3000) % nums.len()]
+pub fn part_one(input: &str) -> i64 {
+    let nums = parse(input);
+    let mut order = (0..nums.len() as i64).collect::<Vec<_>>();
+    let mixed = mix(&nums, &mut order);
+
+    let p = mixed.iter().position(|&n| n == 0).unwrap();
+    mixed[(p + 1000) % mixed.len()]
+        + mixed[(p + 2000) % mixed.len()]
+        + mixed[(p + 3000) % mixed.len()]
+}
+
+pub fn part_two(input: &str) -> i64 {
+    let nums: Vec<i64> = parse(input).iter().map(|n| n * 811589153).collect();
+    let mut order = (0..nums.len() as i64).collect::<Vec<_>>();
+    let mut mixed = Vec::new();
+    for _ in 0..10 {
+        mixed = mix(&nums, &mut order);
+    }
+
+    let p = mixed.iter().position(|&n| n == 0).unwrap();
+    mixed[(p + 1000) % mixed.len()]
+        + mixed[(p + 2000) % mixed.len()]
+        + mixed[(p + 3000) % mixed.len()]
 }
 
 #[cfg(test)]
@@ -45,5 +55,15 @@ mod tests {
     #[test]
     fn challenge_part_one() {
         assert_eq!(part_one(include_str!("../challenge.txt")), 8028);
+    }
+
+    #[test]
+    fn example_part_two() {
+        assert_eq!(part_two(include_str!("../example.txt")), 1623178306);
+    }
+
+    #[test]
+    fn challenge_part_two() {
+        assert_eq!(part_two(include_str!("../challenge.txt")), 8798438007673);
     }
 }
